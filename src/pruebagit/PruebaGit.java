e3add32f90d4cce055524278ac4f196eead31923
Jhonattan <Jhonattan@Tornado>
2014-03-12 14:18:07 +0100

    prueba git1.0.2

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -12,8 +12,20 @@ public class PruebaGit {
 
     /**
      * @param args the command line arguments
+     * v1.0.2
      */
     public static void main(String[] args) {
         // TODO code application logic here
+   
+//    esto es una pprueba de como subir proyectos en el github etc.
+//    
+//    saludos hermanos.
+//    
+//    
+//    
+    
+    
+    
     }
+    
 }
